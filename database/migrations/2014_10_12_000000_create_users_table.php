bf363358e2ab586db4f33699ac04ff390a6e168d
Marhardal <martindharawa@gmail.com>
2023-05-29 08:53:12 +0200

    Added Typre migration which has a relationship with the users table with the goal of removing the seekers table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('username')->unique();
+            $table->unsignedInteger('type_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
